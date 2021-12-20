56df5d3ce4d227ed546a6769718630828b0c7675
Dineo Mathibela <dineomathibela161@gmail.com>
2021-12-20 15:56:24 +0200

    nav1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,9 +6,36 @@ import { DiCssdeck } from 'react-icons/di';
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+
+  //navigation bar (horzontal)
+  <Container>
+      <Div1>
+          <Link href="/">
+            <a style={{display: "flex", alignItems: "center", color: "white"}}>
+              <DiCssdeck size="3rem" /> <span>Student Portfolio</span>
+            </a>
+          </Link>
+  </Div1>
+  <Div2>
+    <li>
+      <Link href="#projects">
+        <NavLink>Projects</NavLink>
+      </Link>
+    </li>
+    <li>
+      <Link href="#tech">
+        <NavLink>Technologies</NavLink>
+      </Link>
+    </li>
+    <li>
+      <Link href="#about">
+        <NavLink>About</NavLink>
+      </Link>
+    </li>
+
+  </Div2>
+
+  </Container>
 );
 
 export default Header;
